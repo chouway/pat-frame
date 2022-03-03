196074da453fe199d25533ffdcc8d0aeb8456acf
chouway <279704064@qq.com>
2022-03-04 00:31:51 +0800

    爬虫初步应用

--- pat-app/pat-poetry/pat-poetry-synch/src/test/java/com/pat/app/poetry/synch/simple/PageTest.java
+++ pat-app/pat-poetry/pat-poetry-synch/src/test/java/com/pat/app/poetry/synch/simple/PageTest.java
@@ -18,7 +18,15 @@ import java.util.List;
  */
 @Slf4j
 public class PageTest {
-
+    /**
+     * 00:30:27.794 [main] INFO com.pat.app.poetry.synch.simple.PageTest - baidubaikeSearchKey-->resultTtiles=[<a class="result-title" href="https://baike.baidu.com/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/2356313" target="_blank"><em>度关山(曹操</em>诗作) - <em>百度百科</em></a>, <a class="result-title" href="/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/22084627" target="_blank"><em>度关山(</em>柳恽诗作) - <em>百度百科</em></a>, <a class="result-title" href="/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/22546029" target="_blank"><em>度关山(</em>王训诗作) - <em>百度百科</em></a>, <a class="result-title" href="/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/22025160" target="_blank"><em>度关山(</em>萧纲诗作) - <em>百度百科</em></a>, <a class="result-title" href="/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/22104194" target="_blank"><em>度关山(</em>戴暠诗作) - <em>百度百科</em></a>, <a class="result-title" href="https://baike.baidu.com/item/%E6%9B%B9%E6%93%8D/2364108" target="_blank"><em>曹操(</em>游戏《英雄杀》中的武将牌) - <em>百度百科</em></a>]
+     * 00:30:27.828 [main] INFO com.pat.app.poetry.synch.simple.PageTest - baidubaikeSearchKey-->href=https://baike.baidu.com/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/2356313,title=度关山(曹操诗作) - 百度百科
+     * 00:30:27.830 [main] INFO com.pat.app.poetry.synch.simple.PageTest - baidubaikeSearchKey-->href=/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/22084627,title=度关山(柳恽诗作) - 百度百科
+     * 00:30:27.832 [main] INFO com.pat.app.poetry.synch.simple.PageTest - baidubaikeSearchKey-->href=/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/22546029,title=度关山(王训诗作) - 百度百科
+     * 00:30:27.838 [main] INFO com.pat.app.poetry.synch.simple.PageTest - baidubaikeSearchKey-->href=/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/22025160,title=度关山(萧纲诗作) - 百度百科
+     * 00:30:27.846 [main] INFO com.pat.app.poetry.synch.simple.PageTest - baidubaikeSearchKey-->href=/item/%E5%BA%A6%E5%85%B3%E5%B1%B1/22104194,title=度关山(戴暠诗作) - 百度百科
+     * 00:30:27.847 [main] INFO com.pat.app.poetry.synch.simple.PageTest - baidubaikeSearchKey-->href=https://baike.baidu.com/item/%E6%9B%B9%E6%93%8D/2364108,title=曹操(游戏《英雄杀》中的武将牌) - 百度百科
+     */
     @Test
     public void baidubaikeSearchKey(){
         String url = "https://baike.baidu.com/search?word=度关山(曹操) - 百度百科&pn=0&rn=0&enc=utf8";
