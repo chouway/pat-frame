20929e51d4bede57728f258bef5721654f0d3d5e
chouway <279704064@qq.com>
2022-03-14 16:03:42 +0800

    查询展示

--- pat-app/pat-poetry/pat-poetry-web/src/components/BaishMain.vue
+++ pat-app/pat-poetry/pat-poetry-web/src/components/BaishMain.vue
@@ -1,6 +1,6 @@
 <template>
   <el-row justify="center">
-    <el-col :span="6">
+    <el-col :span="5">
       <div class="poet-autocomplete">
 
       <el-autocomplete  :fetch-suggestions="querySearchAsync" v-model.lazy="searchKey" placeholder="中华古典文集"  size="large"
@@ -10,13 +10,39 @@
       </div>
     </el-col>
 
+  </el-row>
+
+  <div>
+    <v-for>
+
+    </v-for>
+  <el-row style="margin-top:20px" justify="center" :gutter="20">
+
+      <el-col v-for="o in 1" :key="o" :span="4" style="margin:10px 20px;">
+        <el-card class="box-card" size="large" style="padding:5px">
+          <template #header>
+            <div class="card-header">
+              <span>Card name</span>
+              <el-button class="button" type="text">Operation button</el-button>
+            </div>
+          </template>
+          <div v-for="o in 1" :key="o" class="text item">{{ 'List item ' + o }}</div>
+        </el-card>
+      </el-col>
+
 
   </el-row>
 
-
+  <el-row justify="center" style="margin-top:20px">
+    <el-pagination background layout="prev, pager, next" :total="1000">
+    </el-pagination>
+  </el-row>
+  </div>
 </template>
 
 <script>
+import {ElMessage} from "element-plus";
+
 export default {
   name: "BaishMain",
   data(){
@@ -39,12 +65,12 @@ export default {
               }
               cb(results);
             }else{
-              console.error(res.data.message);
+              ElMessage.warning(res.data.message);
             }
           }
       ).catch(
-          (error) => {
-          console.error("error" + error);
+          () => {
+            ElMessage.error("server error");
         }
       )
 
