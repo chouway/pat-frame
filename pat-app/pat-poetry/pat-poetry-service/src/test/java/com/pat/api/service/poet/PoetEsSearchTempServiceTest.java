c13fc8a92a0aa8aba9b5f5e3aed421b7f1951ea2
chouway <279704064@qq.com>
2022-03-15 12:36:26 +0800

    待优化

--- pat-app/pat-poetry/pat-poetry-service/src/test/java/com/pat/api/service/poet/PoetEsSearchTempServiceTest.java
+++ pat-app/pat-poetry/pat-poetry-service/src/test/java/com/pat/api/service/poet/PoetEsSearchTempServiceTest.java
@@ -5,8 +5,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.JSONPath;
-import com.alibaba.fastjson.parser.Feature;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.pat.api.constant.PoetCharConstant;
 import com.pat.api.constant.PoetIndexConstant;
 import com.pat.api.constant.PoetSearchTempConstant;
 import com.pat.api.service.PoetServiceTest;
@@ -21,8 +21,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * PoetEsSearchTempServiceTest
@@ -168,7 +166,7 @@ public class PoetEsSearchTempServiceTest extends PoetServiceTest {
         prefixSuggest.setField("prefixPinyin");
         prefixSuggest.setSize(size);
         prefixSuggest.setKeyword(PinyinUtil.getFirstLetter(keyword, " "));
-        prefixSuggest.setEnd(PoetSearchTempConstant.CHAR_EMPTY);
+        prefixSuggest.setEnd(PoetCharConstant.CHAR_EMPTY);
         suggestInfoMOs.add(prefixSuggest);
 
         poetSuggestPageMO.setSuggestInfos(suggestInfoMOs);
--- pat-app/pat-poetry/pat-poetry-web/src/components/BaishMain.vue
+++ pat-app/pat-poetry/pat-poetry-web/src/components/BaishMain.vue
@@ -3,59 +3,88 @@
     <el-col :span="5">
       <div class="poet-autocomplete">
 
-      <el-autocomplete  :fetch-suggestions="querySearchAsync" v-model.lazy="searchKey" placeholder="中华古典文集"  size="large"
-                        :hide-loading="true" class="input-with-select" maxlength="100"  :suffix-icon="suffixIcon" clearable="true">
-        <template #append><el-button>搜索</el-button></template>
+      <el-autocomplete :fetch-suggestions="suggestAsync" v-model.lazy="searchKey" placeholder="中华古典文集" size="large"
+                       :hide-loading="true" class="input-with-select" maxlength="100" :suffix-icon="suffixIcon" clearable="true">
+        <template #append><el-button @click="searchAsync">搜索</el-button></template>
       </el-autocomplete>
       </div>
     </el-col>
 
   </el-row>
 
-  <div>
-    <v-for>
+    <div  v-show="poetSearchResultBOs.total>0">
 
-    </v-for>
-  <el-row style="margin-top:40px" justify="center" :gutter="30">
+        <el-row style="margin-top:40px" justify="center" :gutter="30">
+          <el-col v-for="info in poetInfoBOs" :key="'i'+info.id" :span="4">
+            <el-card class="box-card" size="large" style="padding:5px">
+              <template #header>
+                <div class="card-header">
+                  <span>Card name</span>
+                  <el-button class="button" type="text">{{ info.title }}</el-button>
+                </div>
+              </template>
+              <div v-for="(p,index) in info.paragraphs" :key="'p'+index" class="text item">{{ p }}</div>
+            </el-card>
+          </el-col>
+        </el-row>
 
-      <el-col v-for="o in 3" :key="o" :span="4">
-        <el-card class="box-card" size="large" style="padding:5px">
-          <template #header>
-            <div class="card-header">
-              <span>Card name</span>
-              <el-button class="button" type="text">Operation button</el-button>
-            </div>
-          </template>
-          <div v-for="o in 2" :key="o" class="text item">{{ 'List item ' + o }}</div>
-        </el-card>
-      </el-col>
+        <el-row justify="center" style="margin-top:30px">
+          <el-pagination background layout="prev, pager, next" :total="1000" :page-sizes="10" :hide-on-single-page="true">
+          </el-pagination>
+        </el-row>
 
-
-  </el-row>
-
-  <el-row justify="center" style="margin-top:30px">
-    <el-pagination background layout="prev, pager, next" :total="1000" :page-sizes="10" :hide-on-single-page="true">
-    </el-pagination>
-  </el-row>
-  </div>
+    </div>
 </template>
 
 <script>
 import {ElMessage} from "element-plus";
-
 export default {
+
   name: "BaishMain",
   data(){
     return {
       searchKey: "",
       suffixIcon: "search",
       poetSearchResultBOs:{
-
+        total:0,
+        pageSize:10,
+        pageNum:1,
+        propKeys:[],
+        poetAggsBO:[],
+        poetInfoBOs:[]
       }
     }
   },
+  mounted() {
+    this.$nextTick(function () {
+        this.searchAsync();
+
+    })
+  },
   methods:{
-    querySearchAsync(searchKey,cb){
+    searchAsync(){
+      let that = this;
+      this.$http.post("/api/poet/es/search",{keyword:this.searchKey})
+          .then(
+              (res) => {
+                console.info(that.poetSearchResultBOs.total);
+                console.info("data"+ res);
+                if(res.data.success){
+                  console.info(that);
+                  that.$set(that.poetSearchResultBOs.total, 1);
+                  console.info(that.poetSearchResultBOs.total);
+                }else{
+                  ElMessage.warning(res.data.message);
+                }
+              }
+          ).catch(
+          (err) => {
+            console.error("err"+err);
+            ElMessage.error("server error");
+          }
+      )
+    },
+    suggestAsync(searchKey, cb){
         console.info("searchKey="+searchKey);
       this.$http.post("/api/poet/es/suggest",{keyword:this.searchKey})
       .then(
@@ -76,7 +105,6 @@ export default {
             ElMessage.error("server error");
         }
       )
-
     }
   },
   watch:{
