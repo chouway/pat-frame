34488efb655ac36237d7cb4ef7eb5fe2e45d38fa
chouway <279704064@qq.com>
2022-03-18 17:06:26 +0800

    排版

--- pat-app/pat-poetry/pat-poetry-web/src/components/BaishMain.vue
+++ pat-app/pat-poetry/pat-poetry-web/src/components/BaishMain.vue
@@ -2,7 +2,6 @@
   <el-row justify="center" v-show="!fullScreen">
     <el-col :span="5">
       <div class="poet-autocomplete">
-
         <el-autocomplete :fetch-suggestions="suggestAsync" v-model.lazy="searchKey" placeholder="中华古典文集" size="large"
                          :hide-loading="true" class="input-with-select" maxlength="100" :suffix-icon="suffixIcon"
                          clearable="true">
@@ -12,45 +11,14 @@
         </el-autocomplete>
       </div>
     </el-col>
+
   </el-row>
 
 
-  <div v-show="fullScreen" class="poet-fullScreen" style="margin-top:12px">
-    <el-row style="margin-top:12px" justify="center">
-      <el-col :span="20">
-        <el-card class="box-card" size="large">
-          <template #header>
-            <div class="card-header">
-              <span style="margin-left: 100px;;">《{{ targetCardRef.title }}》  {{ targetCardRef.author }}</span>
-              <el-button type="text" @click="fullScreen=false" style="float:right;padding-bottom: 35px;">
-                <el-icon>
-                  <Minus/>
-                </el-icon>
-              </el-button>
-              <el-button type="text" title="百科" style="float:right;padding-bottom: 35px;margin-right:10px;">
-                <el-icon>
-                  <Document/>
-                </el-icon>
-              </el-button>
-            </div>
-          </template>
-          <el-scrollbar>
-            <div v-for="(p,index) in targetCardRef.paragraphs" :key="'p'+index" class="text item"
-                 style="margin:8px 0px;padding:10px 0px;">{{ p }}
-            </div>
-          </el-scrollbar>
-        </el-card>
-      </el-col>
-    </el-row>
-  </div>
 
   <div v-show="poetResult.total==0&&!fullScreen" style="margin-top:12px">
-
-
     <el-row justify="center">
-
       <el-empty description="未找到"/>
-
     </el-row>
   </div>
   <div v-show="poetResult.total>0&&!fullScreen" ref="mainPoetRef">
@@ -91,6 +59,34 @@
       </el-pagination>
     </el-row>
 
+    <div v-show="fullScreen" class="poet-fullScreen" style="margin-top:12px">
+      <el-row style="margin-top:12px" justify="center">
+        <el-col :span="20">
+          <el-card class="box-card" size="large">
+            <template #header>
+              <div class="card-header">
+                <span style="margin-left: 100px;;">《{{ targetCardRef.title }}》  {{ targetCardRef.author }}</span>
+                <el-button type="text" @click="fullScreen=false" style="float:right;padding-bottom: 35px;">
+                  <el-icon>
+                    <Minus/>
+                  </el-icon>
+                </el-button>
+                <el-button type="text" title="百科" style="float:right;padding-bottom: 35px;margin-right:10px;">
+                  <el-icon>
+                    <Document/>
+                  </el-icon>
+                </el-button>
+              </div>
+            </template>
+            <el-scrollbar>
+              <div v-for="(p,index) in targetCardRef.paragraphs" :key="'p'+index" class="text item"
+                   style="margin:8px 0px;padding:10px 0px;">{{ p }}
+              </div>
+            </el-scrollbar>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
 
   </div>
   <!--      <el-footer>
