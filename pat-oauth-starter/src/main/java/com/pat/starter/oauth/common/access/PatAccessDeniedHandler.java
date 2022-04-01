b2a22b04e4a52faeb043c1aa16d5180903a5e075
chouway <279704064@qq.com>
2022-04-01 17:02:41 +0800

    优化deny描述

--- pat-oauth-starter/src/main/java/com/pat/starter/oauth/common/access/PatAccessDeniedHandler.java
+++ pat-oauth-starter/src/main/java/com/pat/starter/oauth/common/access/PatAccessDeniedHandler.java
@@ -2,6 +2,8 @@ package com.pat.starter.oauth.common.access;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pat.api.bo.ResponseBO;
+import com.pat.api.bo.ResultBO;
+import com.pat.api.constant.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -17,16 +19,28 @@ import java.io.IOException;
 @Slf4j
 public class PatAccessDeniedHandler implements AccessDeniedHandler {
 
+    /**
+     * HttpServletResponse.SC_FORBIDDEN
+     * @param request
+     * @param response
+     * @param accessDeniedException
+     * @throws IOException
+     * @throws ServletException
+     */
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        ResponseBO resBO = new ResponseBO(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getMessage(), null);
-        writerError(resBO,response);
+        log.info("handle-->accessDeniedException.msg={}", accessDeniedException.getMessage());
+        ResultBO resultBO = new ResultBO();
+        resultBO.setSuccess(false);
+        resultBO.setMessage(ErrorCode.OAUTH_DENIED.getName());
+        resultBO.setCode(ErrorCode.OAUTH_DENIED.getCode());
+        writerError(resultBO,response);
     }
 
-    public void writerError(ResponseBO responseBO, HttpServletResponse response) throws IOException {
-        response.setStatus(responseBO.getStatus());
+    public void writerError(ResultBO resultBO, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType("application/json,charset=utf-8");
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(response.getOutputStream(), responseBO);
+        objectMapper.writeValue(response.getOutputStream(), resultBO);
     }
 }
