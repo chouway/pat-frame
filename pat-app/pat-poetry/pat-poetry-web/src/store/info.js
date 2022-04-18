e3b2d87ac1c5bed1f5f3e849c6fedd69237c15ad
chouway <279704064@qq.com>
2022-04-18 15:15:23 +0800

    待整合完整pinia

--- pat-app/pat-poetry/pat-poetry-web/src/store/info.js
+++ pat-app/pat-poetry/pat-poetry-web/src/store/info.js
@@ -25,7 +25,7 @@ export const userStore = defineStore({
         enabled: true,
         strategies: [
             {
-                key: "users",
+                key: "poetry",
                 storage: localStorage,
             },
         ],
